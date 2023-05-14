44c68b03bf0f2baa498a90ca70eb0ff2d04a4d50
vellons <vellons@users.noreply.github.com>
2023-05-14 15:54:33 +0200

    Descrizione algo schedulazione + milgioramenti cap 2 e 3

--- code/scheduling.py
+++ code/scheduling.py
@@ -2,8 +2,8 @@
 # Based on the number of hour available for the update
 # the server with more hour available 
 # (or maintenance_window==None) it will be scheduled last
-...
 
+...
 server_key_in_order = sorted(
     self.items_data.keys(),
     key=lambda k: self.items_data[k].priority_score,
@@ -14,21 +14,20 @@ server_key_cannot_schedule = []
 for key in server_key_in_order:
     server = self.items_data[key]
     server.slot_start = None
-...
+    ...
 
-available_slots = self.calendar.get_list_of_slot_to_try(server)
-for slot_start in available_slots:
-    is_slot_ok = server.is_slot_ok_for_maintenance(
-        slot_start, slot_start + self.SLOT_DURATION_IN_MINUTE * 60 - 1
-    )
-    is_slot_ok = True if skip_maintenance_check else is_slot_ok
-    if is_slot_ok:
-        server.apply_slot(slot_start)
-        self.calendar.add_to_calendar(slot_start, server)
-        break
+    available_slots = self.calendar.get_list_of_slot_to_try(server)
+    for slot_start in available_slots:
+        is_slot_ok = server.is_slot_ok_for_maintenance(
+            slot_start, slot_start + self.SLOT_DURATION_IN_MINUTE * 60 - 1
+        )
+        if is_slot_ok:
+            server.apply_slot(slot_start)
+            self.calendar.add_to_calendar(slot_start, server)
+            break
 
-# If a slot was not found ServerItem.slot_start is None
-if server.slot_start is None and server.to_schedule:
-    server_key_not_found_a_schedule.append(key)
+    # If a slot was not found ServerItem.slot_start is None
+    if server.slot_start is None and server.to_schedule:
+        server_key_not_found_a_schedule.append(key)
 
-...
+    ...
